0545cd82f63d82a0b875d961194365bcb16d23c3
mycowbei69 <C113181144@nkust.edu.tw>
2026-03-05 11:10:13 +0800

    ass

---
+++ ex02.java
@@ -0,0 +1,14 @@
+public class ex02 {
+    public static void main(String[] args) {
+        int grade = 50;
+        char type = 'm';
+        if (grade >= 60 && type == 'm') {
+            System.out.println("課程:" + type + "\n成績及格:" + grade);
+
+        } else {
+            System.out.println("課程不正確或成績不及格");
+        }
+
+    }
+
+}
